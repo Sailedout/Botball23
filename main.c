3536a412b6f3c6960beddfc19e66511658d4b73e
Sailedout <102672490+Sailedout@users.noreply.github.com>
2023-03-24 17:37:29 -0500

    Add files via upload
    Took out comments on code

--- main.c
+++ main.c
@@ -1,20 +1,20 @@
 #include <kipr/wombat.h>
 int main()
 {
-    //create_connect(); //connects to create
+    create_connect(); //connects to create
    
     //wait_for_light(0); // lets you callabrate the light
    
     //shut_down_in(119); //shuts down in 2 minutes
                      
-    //enable_servos();  // starts the servos
+    enable_servos();  // starts the servos
     
-    //set_servo_position(1,1900);// opens arm
+    set_servo_position(1,1900);// opens arm
     
-        //create_drive_direct(400,400);
-    //msleep(3000);
+    create_drive_direct(400,400);
+    msleep(3000);
         
-        //set_servo_position(1,900);// closes arm
+    set_servo_position(1,900);// closes arm
        
     
  
@@ -26,9 +26,9 @@ int main()
    
     
     
-    //disable_servos();
-    //create_stop();
-    //create_disconnect();
-    //return 0;
+    disable_servos();
+    create_stop();
+    create_disconnect();
+    return 0;
 }
 
